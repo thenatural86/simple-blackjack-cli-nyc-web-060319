b19cf7e8a8dc3c8060fd52c4ab7baf548bbf82e2
The Natural <philip.smalls86@gmail.com>
2019-05-19 18:23:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(current_card_total)
   prompt_user
   user_input = get_user_input
   if user_input == 's'
-    card_total
+    current_card_total
   elsif user_input == 'h'
     deal_card += deal_card
   else invalid_command
